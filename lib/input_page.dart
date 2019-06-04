79e3a81aad6316e0dfe57b427385677bd9942422
piladzisi <piladzisi@gmail.com>
2019-06-04 09:10:48 +0300

    card icons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerHeight = 80.0;
+const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -19,12 +24,20 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableCard(
-                    colour: Color(0xFF1D1E33),
+                    colour: activeCardColor,
+                    cardChild: ReusableCardIconColumn(
+                      cardIcon: FontAwesomeIcons.mars,
+                      label: 'male'.toUpperCase(),
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: Color(0xFF1D1E33),
+                    colour: activeCardColor,
+                    cardChild: ReusableCardIconColumn(
+                      cardIcon: FontAwesomeIcons.venus,
+                      label: 'female'.toUpperCase(),
+                    ),
                   ),
                 ),
               ],
@@ -32,38 +45,73 @@ class _InputPageState extends State<InputPage> {
           ),
           Expanded(
               child: ReusableCard(
-            colour: Color(0xFF1D1E33),
+            colour: activeCardColor,
           )),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
                   child: ReusableCard(
-                    colour: Color(0xFF1D1E33),
+                    colour: activeCardColor,
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: Color(0xFF1D1E33),
+                    colour: activeCardColor,
                   ),
                 ),
               ],
             ),
           ),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
         ],
       ),
     );
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+class ReusableCardIconColumn extends StatelessWidget {
+  ReusableCardIconColumn({this.cardIcon, this.label});
 
-  final Color colour;
+  final IconData cardIcon;
+  final String label;
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          cardIcon,
+          size: 80.0,
+        ),
+        SizedBox(height: 15.0),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour, this.cardChild});
+
+  final Color colour;
+  final Widget cardChild;
+  @override
+  Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
